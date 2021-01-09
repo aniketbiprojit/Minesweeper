e84f89b7fe87fcba1bbb630779d34b2ed06f43c1
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-01-09 23:42:37 +0530

    feat: cell

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
+import type { element } from 'svelte/internal'
 import App from './App.svelte'
 
 const rows = 7
-const cols = 7
+const cols = 10
 const cell_size = 40
 
 const app = new App({
@@ -15,5 +16,27 @@ const app = new App({
 const grid_elem = document.getElementById('grid')
 grid_elem.style.height = Math.floor(cell_size * rows).toString() + 'px'
 grid_elem.style.width = Math.floor(cell_size * cols).toString() + 'px'
-console.log(grid_elem.style.height, grid_elem.style.width)
+
+const grid: Array<Array<Cell>> = Array.from(Array(rows), () => new Array(cols))
+
+class Cell {
+	revealed = false
+	block: HTMLElement
+	constructor(block: HTMLElement) {
+		this.block = block
+	}
+}
+
+const cells = document.getElementsByClassName('cell')
+Array.from(cells).forEach((element) => {
+	if (element instanceof HTMLElement) {
+		const cell = new Cell(element)
+		const x = cell.block.dataset.x.toString()
+		const y = cell.block.dataset.y.toString()
+		grid[x][y] = cell
+	}
+})
+
+console.log(grid)
+
 export default app
